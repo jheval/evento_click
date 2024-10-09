1b050740200459b75c632bf5c3bad3d1f2ebbb3c
jval <2113110046@untels.edu.pe>
2024-10-09 14:04:44 -0500

    evento click

---
+++ DAMSem06_EventoClick/app/src/main/java/com/example/eventoclick/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.eventoclick;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        EdgeToEdge.enable(this);
+
+        setContentView(R.layout.activity_main);
+
+
+        Button btn1 = findViewById(R.id.button_primero);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Clic en el Botón 1", Toast.LENGTH_SHORT).show());
+
+        TextView tv2 = findViewById(R.id.textView_segundo);
+        tv2.setOnClickListener(view -> Toast.makeText(this, "Clic en el Texto 2", Toast.LENGTH_SHORT).show());
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
